fe8e4b50b827147ebbcc8a7667219dda71c71e1b
zhangzhiguo <udp@163.com>
2020-08-29 08:56:42 +0800

    first

---
+++ src/main/java/com/SpringCloudEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
+	}
+
+}
